3491adf8b449835bbac6c94a0baf707ca95d9112
이건후 <lgh3806@naver.com>
2022-07-31 17:11:11 +0900

    chat update

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# WebRTC
+# Zoom Clone Coding
 
-Zoom Clone using WebRTC and Websockets
+- WebRTC와 Websockets를 사용한 줌 클론 코딩 
+
+# 1. Target
+
+- WebRTC를 사용한 웹 기술 PJT 전 사전학습
+
+- 웹소켓을 사용한 양방향 채팅서버 구현
+
+# 2. Learned
+
+### Chat
+
+- Websockets, Socket.io 에 대한 학습을 진행.
+
+- Sockec.io 의 emit 함수의 인자는 emit(이름, arg1, arg2, arg3 ... function) 식으로 맨 앞에는 해당 이벤트의 이름을, 맨 마지막에는 실행되는 함수를 적용시켜줘야 한다. (※ 신뢰 할 수 없는 코드는 Backend에 적지 않기. 보안위험)
+
+- 접속 할 시, 방 이름 설정은 숨기고, 채팅기능은 보이도록
+  
+  ![](assets/2022-07-31-14-28-40-image.png)
+
+      (pug로 작성한 html) 
+
+        ![](assets/2022-07-31-14-30-14-image.png)
+
+        enter_room의 이름을 통해 emit 으로 server를 통해 showRoom 함수를 실행시키게 
+
+        된다면 room의 hidden 상태는 true -> false, welcome 상태는 false -> true가 된다.
+
+- 사용자 편의를 위해 초기 화면을 변경
+  ![](assets/2022-07-31-17-04-19-image.png)
+
+- 1:1 채팅 기능 구현 화면
+  ![](assets/2022-07-31-17-04-05-image.png)
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http"
-import WebSocket from "ws"
+import SocketIO from "socket.io"
 import express from "express"
 
 const app = express()
@@ -10,38 +10,64 @@ app.use("/public", express.static(__dirname + "/public"))
 app.get("/", (req, res) => res.render("home"))
 app.get("/*", (req, res) => res.redirect("/"))
 
-const handleListen = () => console.log(`Listening on http//localhost:3000`)
+// const handleListen = () => console.log(`Listening on http//localhost:3000`)
 
-const server = http.createServer(app)
-const wss = new WebSocket.Server({ server })
+const httpServer = http.createServer(app)
+const wsServer = SocketIO(httpServer)
 
-const sockets = []
+// const nickname = socket.nickname
+wsServer.on("connection", (socket) => {
+	socket['nickname'] = 'anon'
+	socket.onAny((event) => {
+		console.log(`Socket Event: ${event}`)
+	})
+	socket.on("enter_room", (roomName, done) => {
+		socket.join(roomName)
+		done()
+		socket.to(roomName).emit('welcome', socket.nickname)
+	})
+	socket.on('disconnecting', () => {
+		socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname))
+	})
+	socket.on('new_message', (msg, room, done) => {
+		socket.to(room).emit('new_message', ` ${socket.nickname}: ${msg}`)
+		done(socket.nickname)
+	})
+	socket.on('nickname', (nickname) => {
+		socket["nickname"] = nickname
 
-wss.on("connection", (socket) => {
-	sockets.push(socket)
-	socket["nickname"] = "Anon"
-	console.log("Connected to Browser ✅")
-	socket.on("close", () => console.log("Disconnected from the Browser ❌"))
-	socket.on("message", (msg) => {
-		const message = JSON.parse(msg)
-		switch (message.type) {
-			case "new_message":
-				console.log(socket.nickname)
-				console.log(message.payload)
-				sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`))
-				break
-			case "nickname":
-				console.log(message.payload)
-				socket["nickname"] = message.payload
-				break
-		}
-
-		// if (message.type === 'new_message') {
-		// 	sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`))
-		// } else if (message.type === 'nickname') {
-		// 	socket["nickname"] = message.payload
-		// }
 	})
 })
 
-server.listen(3000, handleListen)
+// const sockets = []
+// const wss = new WebSocket.Server({ server })
+// wss.on("connection", (socket) => {
+// 	sockets.push(socket)
+// 	socket["nickname"] = "Anon"
+// 	console.log("Connected to Browser ✅")
+// 	socket.on("close", () => console.log("Disconnected from the Browser ❌"))
+// 	socket.on("message", (msg) => {
+// 		const message = JSON.parse(msg)
+// 		switch (message.type) {
+// 			case "new_message":
+// 				console.log(socket.nickname)
+// 				console.log(message.payload)
+// 				sockets.forEach((aSocket) =>
+// 					aSocket.send(`${socket.nickname}: ${message.payload}`))
+// 				break
+// 			case "nickname":
+// 				console.log(message.payload)
+// 				socket["nickname"] = message.payload
+// 				break
+// 		}
+
+// if (message.type === 'new_message') {
+// 	sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`))
+// } else if (message.type === 'nickname') {
+// 	socket["nickname"] = message.payload
+// }
+// 	})
+// })
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen)
